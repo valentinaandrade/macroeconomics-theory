eb86f02d94ad225fc87146675fd27b6a1e0abb26
valentinaandrade <valentinaandrade@uchile.cl>
2022-01-16 04:22:55 -0300

    Update f_2.m

--- practice/ayudantias/ayu2/f_2.m
+++ practice/ayudantias/ayu2/f_2.m
@@ -1,10 +1,12 @@
-function y = f_2(X)
+function F = f_2(X, A, r)
 
 % Define parameters
 x = X(1);
 y = X(2);
 z = X(3);
-r = [1 2 3 4];
 
 % Define f(x)
-y = x -4*y*r + 6z*r - 1; 
+F = [x -4*y*r + 6z*r - 1; -4*x + 5y + 18(2r-1)z - 2; x + 2(r-1)y + 2rz -  3];
+end
+
+
