d07ca92fa7b074c256a4eeb33d6e2125c2372ff7
valentinaandrade <valentinaandrade@uchile.cl>
2022-01-17 02:57:18 -0300

    Update f_2.m

--- practice/ayudantias/ayu2/f_2.m
+++ practice/ayudantias/ayu2/f_2.m
@@ -10,3 +10,13 @@ F = [x -4*y*r + 6z*r - 1; -4*x + 5y + 18(2r-1)z - 2; x + 2(r-1)y + 2rz -  3];
 end
 
 
+syms x y z r 
+eqn1 = x -4*y*r + 6*z*r == 1;
+eqn2 = -4*x + 5*y + 18*(2*r-1)*z == 2;
+eqn3 = x + 2*(r-1)*y + 2*r*z  ==  3;
+
+[A,B] = equationsToMatrix([eqn1, eqn2, eqn3], [x, y, z,r])
+
+rank(A)
+
+X = linsolve(A,B)
